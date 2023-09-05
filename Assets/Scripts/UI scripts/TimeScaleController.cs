8b3eb1278d1f8b2727382905610fd75385f656b7
Evgeniy Opryshko <eopryshko@ronasit.com>
2023-09-05 20:27:42 +0300

    Fixed timescale affecting FixedUpdate count

--- Assets/Scripts/UI scripts/TimeScaleController.cs
+++ Assets/Scripts/UI scripts/TimeScaleController.cs
@@ -11,9 +11,12 @@ public class TimeScaleController : MonoBehaviour
     public float timeScaleBase = 10f;
     public TextMeshProUGUI timeScaleText;
     public Slider timeScaleSlider;
+    
+    private float fixedDeltaTime;
 
     private void Start()
     {
+        fixedDeltaTime = Time.fixedDeltaTime;
         Time.timeScale = 0f;
         timeScaleSlider.onValueChanged.AddListener(OnTimeScaleChanged);
     }
@@ -23,6 +26,7 @@ public class TimeScaleController : MonoBehaviour
         Time.timeScale =
             Mathf.RoundToInt(Mathf.Pow(timeScaleBase,
                 sliderValue - 1)); // 0 -> 0, 1 -> 1, 2 -> 10, 3 -> 100
+        Time.fixedDeltaTime = fixedDeltaTime * Time.timeScale;
         timeScaleText.text = Time.timeScale + "x";
         if (sliderValue != 0) lastNonZeroSliderValue = Mathf.RoundToInt(sliderValue);
     }
